04343f849d436e7477cf58ef35b649ecbd251c1d
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-10 19:40:29 -0500

    summarize

--- restgdf_api/app.py
+++ restgdf_api/app.py
@@ -1,6 +1,11 @@
+import os
 from typing import Optional
 
+from aiohttp import ClientSession
 from fastapi import FastAPI, HTTPException
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
 from pydantic import BaseModel
 from restgdf import Rest
 
@@ -14,28 +19,129 @@ app = FastAPI(
 
 
 class GeoDataFrameResponse(BaseModel):
+    metadata: dict
     data: str  # JSON representation of GeoDataFrame
+
+
+class SummarizedGeoDataFrameResponse(GeoDataFrameResponse):
+    summarized_description: str
+    summary: str
 
 
 class UniqueValuesResponse(BaseModel):
     values: list
 
 
-class ValueCountsResponse(BaseModel):
-    counts: dict
-
-
-class NestedCountResponse(BaseModel):
-    nested_counts: dict
-
-
 @app.post("/getgdf/", response_model=GeoDataFrameResponse)
 async def get_gdf(url: str, token: Optional[str] = None, where: str = "1=1"):
     """Retrieve a GeoDataFrame from an ArcGIS FeatureLayer."""
     try:
-        rest_obj = await Rest.from_url(url, token=token, where=where)
-        gdf = await rest_obj.getgdf()
-        return GeoDataFrameResponse(data=gdf.to_json())  # Convert to JSON
+        with ClientSession() as session:
+            rest_obj = await Rest.from_url(
+                url,
+                token=token,
+                where=where,
+                session=session,
+            )
+            metadata = rest_obj.jsondict
+            gdf = await rest_obj.getgdf()
+            return GeoDataFrameResponse(
+                metadata=metadata,
+                data=gdf.to_json(),
+            )  # Convert to JSON
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error retrieving GeoDataFrame: {str(e)}",
+        )
+
+
+summarize_record_prompt = ChatPromptTemplate.from_messages(
+    (
+        "user",
+        """
+This data was returned by a geospatial API. We've dropped the geometry column, but summarize the data:
+API Name: {name}
+API Description: {description}
+API Data:
+{data}
+""".strip(),
+    ),
+)
+
+summarize_desc_prompt = ChatPromptTemplate.from_messages(
+    (
+        "user",
+        """
+Summarize this description to no more than three sentences:
+{description}
+""".strip(),
+    ),
+)
+summarize_summaries_prompt = ChatPromptTemplate.from_messages(
+    (
+        "user",
+        """
+Combine these individual summaries of features into a single summary of the dataset:
+{summaries}
+""".strip(),
+    ),
+)
+
+
+@app.post("/getgdf/summarize/", response_model=SummarizedGeoDataFrameResponse)
+async def get_summarized_gdf(
+    url: str,
+    token: Optional[str] = None,
+    where: str = "1=1",
+    openai_api_key: Optional[str] = None,
+):
+    """Retrieve a GeoDataFrame from an ArcGIS FeatureLayer, with summary."""
+    openai_api_key = openai_api_key or os.environ.get("OPENAI_API_KEY", None)
+    if openai_api_key is None:
+        raise HTTPException(
+            status_code=501,
+            detail="Summarization is not enabled.",
+        )
+    try:
+        with ClientSession() as session:
+            rest_obj = await Rest.from_url(
+                url,
+                token=token,
+                where=where,
+                session=session,
+            )
+            metadata = rest_obj.jsondict
+            name = metadata["name"]
+            desc = metadata.get("description", "")
+            # summarize desc if it's likely to be more than 3 sentences, based on len()
+            if len(desc) > 300:
+                desc_chain = (
+                    RunnablePassthrough()
+                    | summarize_desc_prompt
+                    | ChatOpenAI(openai_api_key=openai_api_key)
+                    | (lambda output: output.content.strip())
+                )
+                summarized_desc = await desc_chain.ainvoke(desc)
+            else:
+                summarized_desc = desc
+            gdf = await rest_obj.getgdf()
+            dropcols = ["OBJECTID", "geometry"]
+            final_summary_input = gdf.drop(columns=dropcols).to_json()
+            final_summary_chain = (
+                summarize_record_prompt
+                | ChatOpenAI(openai_api_key=openai_api_key)
+                | (lambda output: output.content.strip())
+            )
+            final_summary = await final_summary_chain.ainvoke(
+                dict(name=name, description=summarized_desc, data=final_summary_input),
+            )
+            return SummarizedGeoDataFrameResponse(
+                metadata=metadata,
+                data=gdf.to_json(),
+                summary=final_summary,
+                summarized_description=summarized_desc,
+            )  # Convert to JSON
     except Exception as e:
         raise HTTPException(
             status_code=500,
@@ -52,9 +158,19 @@ async def get_head(
 ):
     """Retrieve a GeoDataFrame from an ArcGIS FeatureLayer."""
     try:
-        rest_obj = await Rest.from_url(url, token=token, where=where)
-        gdf = await rest_obj.headgdf(n)
-        return GeoDataFrameResponse(data=gdf.to_json())  # Convert to JSON
+        with ClientSession() as session:
+            rest_obj = await Rest.from_url(
+                url,
+                token=token,
+                where=where,
+                session=session,
+            )
+            metadata = rest_obj.jsondict
+            gdf = await rest_obj.headgdf(n)
+            return GeoDataFrameResponse(
+                metadata=metadata,
+                data=gdf.to_json(),
+            )  # Convert to JSON
     except Exception as e:
         raise HTTPException(
             status_code=500,
@@ -71,9 +187,19 @@ async def get_sample(
 ):
     """Retrieve a GeoDataFrame from an ArcGIS FeatureLayer."""
     try:
-        rest_obj = await Rest.from_url(url, token=token, where=where)
-        gdf = await rest_obj.samplegdf(n)
-        return GeoDataFrameResponse(data=gdf.to_json())  # Convert to JSON
+        with ClientSession() as session:
+            rest_obj = await Rest.from_url(
+                url,
+                token=token,
+                where=where,
+                session=session,
+            )
+            metadata = rest_obj.jsondict
+            gdf = await rest_obj.samplegdf(n)
+            return GeoDataFrameResponse(
+                metadata=metadata,
+                data=gdf.to_json(),
+            )  # Convert to JSON
     except Exception as e:
         raise HTTPException(
             status_code=500,
@@ -85,94 +211,12 @@ async def get_sample(
 async def get_unique_values(url: str, field: str, token: Optional[str] = None):
     """Get the unique values for a field in an ArcGIS FeatureLayer."""
     try:
-        rest_obj = await Rest.from_url(url, token=token)
-        unique_values = await rest_obj.getuniquevalues(field)
-        return UniqueValuesResponse(values=unique_values)
+        with ClientSession() as session:
+            rest_obj = await Rest.from_url(url, token=token, session=session)
+            unique_values = await rest_obj.getuniquevalues(field)
+            return UniqueValuesResponse(values=unique_values)
     except Exception as e:
         raise HTTPException(
             status_code=500,
             detail=f"Error retrieving unique values: {str(e)}",
         )
-
-
-# @app.post("/getvaluecounts/", response_model=ValueCountsResponse)
-# async def get_value_counts(url: str, field: str, token: Optional[str] = None):
-#     """
-#     Get the value counts for a specific field in an ArcGIS FeatureLayer.
-#
-#     Args:
-#     - url (str): URL of the ArcGIS FeatureLayer.
-#     - field (str): Field name to get value counts from.
-#     - token (str, optional): Authentication token, if required.
-#
-#     Returns:
-#     - ValueCountsResponse: Dictionary of value counts.
-#     """
-#     try:
-#         rest_obj = await Rest.from_url(url, token=token)
-#         value_counts = await rest_obj.getvaluecounts(field)
-#         return ValueCountsResponse(counts=value_counts)
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500,
-#             detail=f"Error retrieving value counts: {str(e)}",
-#         )
-#
-#
-# @app.post("/getnestedcount/", response_model=NestedCountResponse)
-# async def get_nested_count(url: str, fields: list[str], token: Optional[str] = None):
-#     """
-#     Get nested value counts for a combination of fields in an ArcGIS FeatureLayer.
-#
-#     Args:
-#     - url (str): URL of the ArcGIS FeatureLayer.
-#     - fields (list[str]): List of fields to get nested counts.
-#     - token (str, optional): Authentication token, if required.
-#
-#     Returns:
-#     - NestedCountResponse: Dictionary of nested value counts.
-#     """
-#     try:
-#         rest_obj = await Rest.from_url(url, token=token)
-#         nested_counts = await rest_obj.getnestedcount(tuple(fields))
-#         return NestedCountResponse(nested_counts=nested_counts)
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500,
-#             detail=f"Error retrieving nested counts: {str(e)}",
-#         )
-#
-#
-# @app.post("/customquery/", response_model=GeoDataFrameResponse)
-# async def custom_query(
-#     url: str,
-#     where: str,
-#     token: Optional[str] = None,
-#     additional_params: dict[str, str] = Body(...),
-# ):
-#     """
-#     Perform a custom query on an ArcGIS FeatureLayer with additional parameters.
-#
-#     Args:
-#     - url (str): URL of the ArcGIS FeatureLayer.
-#     - where (str): WHERE clause for filtering data.
-#     - token (str, optional): Authentication token, if required.
-#     - additional_params (dict, optional): Additional query parameters.
-#
-#     Returns:
-#     - GeoDataFrameResponse: JSON representation of the GeoDataFrame.
-#     """
-#     try:
-#         rest_obj = await Rest.from_url(
-#             url,
-#             token=token,
-#             where=where,
-#             **additional_params,
-#         )
-#         gdf = await rest_obj.getgdf()
-#         return GeoDataFrameResponse(data=gdf.to_json())
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500,
-#             detail=f"Error performing custom query: {str(e)}",
-#         )
