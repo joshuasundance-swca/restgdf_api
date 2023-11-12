e50dc998ceee63391da165aa4bac96f3b41ec4c5
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-11 23:31:32 -0500

    bump version 3.2.1 -> 3.3.0

--- restgdf_api/app.py
+++ restgdf_api/app.py
@@ -4,7 +4,7 @@ from directory import directory_router
 from layer import layer_router
 from mappingsupport import mappingsupport_router
 
-__version__ = "3.2.1"
+__version__ = "3.3.0"
 
 app = FastAPI(
     title="restgdf_api",
