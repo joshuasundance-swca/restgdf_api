68fea5a04e35d45a5dbf8ad5ae8531a23a8bf6c2
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-12 15:34:16 -0500

    dynamic

--- restgdf_api/make_clone.py
+++ restgdf_api/make_clone.py
@@ -1,14 +1,16 @@
 from typing import Optional
 
+import aiohttp
 from aiohttp import ClientSession
 from fastapi import APIRouter, Depends, HTTPException
 from restgdf import Directory, FeatureLayer
 
 from models import GeoDataFrameResponse, LayersResponse
 from utils import get_session, rasters_from_response, feature_layers_from_response
+from utils import relative_path
 
 
-def make_clone(
+async def make_clone(
     cloned_url: str,
     default_token: Optional[str] = None,
     prefix: str = "/clone",
@@ -100,5 +102,54 @@ def make_clone(
                 status_code=500,
                 detail=f"Error retrieving GeoDataFrame: {str(e)}",
             )
+
+    def create_layer_endpoint(rel_path: str):
+        """Create a closure that captures the relative path."""
+
+        async def endpoint_function(
+            token: Optional[str] = None,
+            where: str = "1=1",
+            session: ClientSession = Depends(get_session),
+        ):
+            """Endpoint function using captured relative path."""
+            try:
+                full_url = f"{cloned_url.strip('/ ')}/{rel_path.strip('/ ')}"
+                rest_obj = await FeatureLayer.from_url(
+                    full_url,
+                    token=token or default_token,
+                    where=where,
+                    session=session,
+                )
+                metadata = rest_obj.jsondict
+                gdf = await rest_obj.getgdf()
+                return GeoDataFrameResponse(
+                    metadata=metadata,
+                    data=gdf.to_json(),
+                )
+            except Exception as e:
+                raise HTTPException(
+                    status_code=500,
+                    detail=f"Error retrieving GeoDataFrame: {str(e)}",
+                )
+
+        return endpoint_function
+
+    async with aiohttp.ClientSession() as session:
+        rest_obj = await Directory.from_url(
+            cloned_url,
+            session=session,
+            token=default_token,
+        )
+        layers_data = feature_layers_from_response(rest_obj.data)
+        for service_name, layers in layers_data.items():
+            for _layer in layers:
+                rel_path = relative_path(_layer["url"], root_url=cloned_url)
+                layer_endpoint = create_layer_endpoint(rel_path)
+                router.add_api_route(
+                    path=rel_path,
+                    endpoint=layer_endpoint,
+                    response_model=GeoDataFrameResponse,
+                    summary=f"Retrieve FeatureLayer for {service_name}/{_layer['name']}",
+                )
 
     return router
--- restgdf_api/vcgov.py
+++ restgdf_api/vcgov.py
@@ -1,7 +1,11 @@
+import asyncio
+
 from make_clone import make_clone
 
-vcgov_router = make_clone(
-    cloned_url="https://maps1.vcgov.org/arcgis/rest/services",
-    prefix="/vcgov",
-    tags=["vcgov"],
+vcgov_router = asyncio.run(
+    make_clone(
+        cloned_url="https://maps1.vcgov.org/arcgis/rest/services",
+        prefix="/vcgov",
+        tags=["vcgov"],
+    ),
 )
