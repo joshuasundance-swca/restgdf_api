09bc454ed98cb4e1b02617c56d22d229a8561589
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-11 20:04:12 -0500

    bump version 3.0.0 -> 3.0.1

--- restgdf_api/app.py
+++ restgdf_api/app.py
@@ -9,7 +9,7 @@ from langchain.schema.runnable import RunnablePassthrough
 from pydantic import BaseModel
 from restgdf import FeatureLayer, Directory
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 app = FastAPI(
     title="restgdf_api",
